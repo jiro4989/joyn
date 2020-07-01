38279e406eba34472fbb66d3f78dc57258a4bdff
jiro4989 <jiroron666@gmail.com>
2020-07-01 12:28:26 +0000

    add splitArgs

--- src/joyn.nim
+++ src/joyn.nim
@@ -1,5 +1,40 @@
+import sequtils
+
 const
   version = "v0.1.0"
+
+type
+  Args = object
+    firstCmd: seq[string]
+    firstFile: string
+    secondCmd: seq[string]
+    secondFile: string
+  InvalidArgsError = object of CatchableError
+
+proc getArgsAndDelete(args: var seq[string], delim: string): seq[string] =
+  var m = args.high
+  for i in 0..m:
+    let arg = args[0]
+    args.delete(0, 0)
+
+    if arg == delim:
+      break
+    result.add(arg)
+
+proc splitArgs(args: seq[string]): Args =
+  if args.len < 1:
+    raise newException(InvalidArgsError, "need args")
+  var args = args
+  let delim = args[0]
+  args.delete(0, 0)
+
+  result.firstCmd = getArgsAndDelete(args, delim)
+  result.secondCmd = getArgsAndDelete(args, delim)
+
+  if args.len != 2:
+    raise newException(InvalidArgsError, "need 2 files in last parts")
+  result.firstFile = args[0]
+  result.secondFile = args[1]
 
 proc joyn(args: seq[string]): int =
   echo args
--- tests/test1.nim
+++ tests/test1.nim
@@ -2,6 +2,39 @@ import unittest
 
 include joyn
 
-suite "suite":
-  test "test":
-    check true
+suite "proc splitArgs":
+  test "normal: first argument is the delimiter of args":
+    check splitArgs(@["/", "echo", "a", "/", "echo", "b", "/", "c.txt", "d.txt"]) ==
+      Args(
+        firstCmd: @["echo", "a"],
+        firstFile: "c.txt",
+        secondCmd: @["echo", "b"],
+        secondFile: "d.txt",
+        )
+  test "normal: first argument is any character":
+    check splitArgs(@[":", "echo", "a", ":", "echo", "b", ":", "c.txt", "d.txt"]) ==
+      Args(
+        firstCmd: @["echo", "a"],
+        firstFile: "c.txt",
+        secondCmd: @["echo", "b"],
+        secondFile: "d.txt",
+        )
+  test "abnormal: the last part must have 2 files":
+    expect(InvalidArgsError):
+      discard splitArgs(@["/", "echo", "a", "/", "echo", "b", "/"])
+    expect(InvalidArgsError):
+      discard splitArgs(@["/", "echo", "a", "/", "echo", "b", "/", "c.txt"])
+    expect(InvalidArgsError):
+      discard splitArgs(@["/", "echo", "a", "/", "echo", "b", "/", "c.txt", "d.txt", "f.txt"])
+  test "abnormal: args must have 3 parts":
+    expect(InvalidArgsError):
+      discard splitArgs(@["/", "echo", "a", "/", "echo", "b"])
+    expect(InvalidArgsError):
+      discard splitArgs(@["echo", "a", "echo", "b"])
+  test "abnormal: need args":
+    expect(InvalidArgsError):
+      discard splitArgs(@[])
+    expect(InvalidArgsError):
+      discard splitArgs(@["/"])
+    expect(InvalidArgsError):
+      discard splitArgs(@["/", "/"])
